78946d624319e222ee5af750818224f0ac8e1d23
AgustinMassieri <agmassieri00@gmail.com>
2023-06-03 16:44:23 -0300

    status in reservations

--- routes/reservations.js
+++ routes/reservations.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { getReservations, createReservation, getReservationsByUserId } = require('../controllers/reservations');
+const { getReservations, createReservation, getReservationsByUserId, cancelReservation } = require('../controllers/reservations');
 
 const checkRol = require('../middleware/role');
 
 router.get("/",  getReservations);
 router.get("/:id",  getReservationsByUserId);
-router.post("/", createReservation);
+router.post("/", createReservation);    
+router.put("/:id", cancelReservation);
 
 module.exports = router;
